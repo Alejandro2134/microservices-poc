fb7cdee5a85aa28fcba8fb21f2fb5baf1a06ad1b
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2025-03-23 22:00:07 -0500

    feat: rabbitMQ integration micro 1

--- microservice-1/src/app.controller.ts
+++ microservice-1/src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('MICROSERVICE1_SERVICE') private client: ClientProxy,
+  ) {}
 
   @Get()
   getHello(): string {
+    this.client.emit('hello', { data: 'Hola, microservicio 2!' });
     return this.appService.getHello();
   }
 }
--- microservice-1/src/app.module.ts
+++ microservice-1/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'MICROSERVICE1_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://kalo:kalo@localhost:5672'],
+          queue: 'micro-poc',
+          queueOptions: {
+            durable: true,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
